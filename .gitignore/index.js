493641b132101ea84e29590fa529b0f2104ddc6f
matteoJS <widep.woud@gmail.com>
2018-11-11 19:01:18 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const yt = require('youtube-audio-stream');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -6,8 +7,17 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  if (msg.content.indexOf('pourparlers') > -1) {
+    msg.reply('Article 8 du code des pirates - Une personne placé sous la protection des pourparlers devra être conduite saine et sauve au capitaine ennemi pour entamer les négociations.');
+  }
+  if (msg.content.indexOf('!play ') > -1) {
+    let s = msg.content.substr(5);
+    let channel = msg.guild.channels.filter(function (channel) {return (channel.type == 'voice');}).first();
+    channel.join().then(function (conn) {
+      let stream = yt(s);
+      conn.playStream(stream).on('end', function () {conn.disconnect()});
+    });
+    msg.reply('Let\'s go !'); 
   }
 });
 
